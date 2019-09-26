e26fa4ff4a5c662abc6aa792cf71abcaa124106e
nadimsheikh <nadim.sheikh.07@gmail.com>
2019-09-26 15:56:16 +0530

    change direction

--- example/src/App.js
+++ example/src/App.js
@@ -29,7 +29,7 @@ class App extends React.Component {
       handleAction: () => {/* do something... */ },
       close: true,
       variant: variant,
-      direction: { vertical: 'bottom', horizontal: 'right' }
+      direction: { vertical: 'bottom', horizontal: 'center' }
     }
     snackbar.show(options)
   }
